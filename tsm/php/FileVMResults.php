8b17d953cc0fe6c937b812ec855deaec9b8c5535
kafruhs <alexander.faercher@gmx.de>
2015-10-12 15:56:23 +0200

    new tsm module

---
+++ tsm/php/FileVMResults.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 12.10.2015
+ * Time: 15:43
+ */
+class BackupResult extends BaseObject
+{
+    protected $table = "backupResult";
+
+
+}
